1950104e1a89bff5b376a1b1b8585d3afbf167a9
Joao Victor Dias <victorcostadev@gmail.com.br>
2024-07-10 00:42:00 -0300

    Remove UseNavigation from AddCartButton Component

--- src/components/AddCartButton/AddCartButton.tsx
+++ src/components/AddCartButton/AddCartButton.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 import styles from './AddCartButton.style';
 
