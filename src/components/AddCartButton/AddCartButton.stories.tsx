f748aca542d09b120e2ca0135d60ed8e2826075c
Joao Victor Dias <victorcostadev@gmail.com.br>
2024-07-10 15:24:40 -0300

    Improve AddCartButton storybook

--- src/components/AddCartButton/AddCartButton.stories.tsx
+++ src/components/AddCartButton/AddCartButton.stories.tsx
@@ -1,43 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Meta, StoryObj } from '@storybook/react-native';
-import { withKnobs } from '@storybook/addon-knobs';
-import { action } from '@storybook/addon-actions';
-import AddCartButton, {AddCartButtonTypes} from './AddCartButton'; // ajuste o caminho conforme necessário
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import AddCartButton, { AddCartButtonTypes } from './AddCartButton';
 
-const meta: Meta = {
+export default {
   title: 'AddCartButton',
   component: AddCartButton,
-  decorators: [withKnobs],
+} as ComponentMeta<typeof AddCartButton>;
+
+const Template: ComponentStory<typeof AddCartButton> = (args) => <AddCartButton {...args} />;
+
+export const Add = Template.bind({});
+Add.args = {
+  type: AddCartButtonTypes.Add,
+  onPress: () => {},
 };
 
-export default meta;
-
-type Story = StoryObj<typeof AddCartButton>;
-
-export const Default: Story = {
-  args: {
-    type: AddCartButtonTypes.Add,
-    onPress: action('Add to Cart'),
-  },
-  render: (args) => (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <View style={{height: 200}}>
-        <AddCartButton {...args} />
-      </View>
-    </View>
-  ),
-};
-export const Secondary: Story = {
-  args: {
-    type: AddCartButtonTypes.Remove,
-    onPress: action('Remove to Cart'),
-  },
-  render: (args) => (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <View style={{height: 200}}>
-        <AddCartButton {...args} />
-      </View>
-    </View>
-  ),
+export const Remove = Template.bind({});
+Remove.args = {
+  type: AddCartButtonTypes.Remove,
+  onPress: () => {},
 };
