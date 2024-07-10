e706d544fca50d85e295e7b971a5ef5ae18a5594
Joao Victor Dias <victorcostadev@gmail.com.br>
2024-07-10 15:24:50 -0300

    Improve CartButton storybook

--- src/components/CartButton/CartButton.stories.tsx
+++ src/components/CartButton/CartButton.stories.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { Meta, StoryObj } from '@storybook/react-native';
-import { withKnobs, number } from '@storybook/addon-knobs';
-import { action } from '@storybook/addon-actions';
-import { NavigationContainer } from '@react-navigation/native';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import CartButton from './CartButton';
 import { createStackNavigator } from '@react-navigation/stack';
-import CartButton from './CartButton'; // ajuste o caminho conforme necessário
+import { NavigationContainer } from '@react-navigation/native';
+import { Text, View } from 'react-native';
+
+export default {
+  title: 'CartButton',
+  component: CartButton,
+} as ComponentMeta<typeof CartButton>;
 
 const Stack = createStackNavigator();
 
@@ -20,30 +23,14 @@ const MockedNavigator = ({ component }: { component: React.FC }) => {
   );
 };
 
-const meta: Meta = {
-  title: 'CartButton',
-  component: CartButton,
-  decorators: [withKnobs],
+const Template: ComponentStory<typeof CartButton> = (args) => <MockedNavigator component={() => <CartButton {...args} />} />
+
+export const EmptyCart = Template.bind({});
+EmptyCart.args = {
+  cartCount: 0,
 };
 
-export default meta;
-
-type Story = StoryObj<typeof CartButton>;
-
-export const Default: Story = {
-  args: {
-    cartCount: 3,
-  },
-  render: (args) => (
-    <MockedNavigator component={() => <CartButton {...args} />} />
-  ),
-};
-
-export const Secondary: Story = {
-  args: {
-    cartCount: 0,
-  },
-  render: (args) => (
-    <MockedNavigator component={() => <CartButton {...args} />} />
-  ),
+export const FilledCart = Template.bind({});
+FilledCart.args = {
+  cartCount: 3,
 };
