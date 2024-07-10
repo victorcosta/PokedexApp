06fa2effabaef27e420774210099c2eeb38346c2
Joao Victor Dias <victorcostadev@gmail.com.br>
2024-07-10 15:25:06 -0300

    Improve CartItem storybook

--- src/components/CartItem/CartItem.stories.tsx
+++ src/components/CartItem/CartItem.stories.tsx
@@ -1,33 +1,16 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Meta, StoryObj } from '@storybook/react-native';
-import { withKnobs, text } from '@storybook/addon-knobs';
-import { action } from '@storybook/addon-actions';
-import CartItem from './CartItem'; // ajuste o caminho conforme necessário
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import CartItem from './CartItem';
 
-const meta: Meta = {
+export default {
   title: 'CartItem',
   component: CartItem,
-  decorators: [withKnobs],
-};
+} as ComponentMeta<typeof CartItem>;
 
-export default meta;
+const Template: ComponentStory<typeof CartItem> = (args) => <CartItem {...args} />;
 
-type Story = StoryObj<typeof CartItem>;
-
-export const Default: Story = {
-  args: {
-    product: {
-      name: text('Product Name', 'Pikachu'),
-      url: text('Product URL', 'https://pokeapi.co/api/v2/pokemon/25/'),
-    },
-    removeFromCart: action('Remove From Cart'),
-  },
-  render: (args) => (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <View style={{height: 200}}>
-        <CartItem {...args} />
-      </View>
-    </View>
-  ),
+export const Default = Template.bind({});
+Default.args = {
+  product: { name: 'Bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' },
+  removeFromCart: () => {},
 };
